79e181a70db8df3a880ee03c36e49e4da7c3d0e4
Sudarshan uprety <sudarshan.uprety@bottle.com.np>
2023-04-11 16:38:25 +0545

    upto Q.8

--- django_northwind.py
+++ django_northwind.py
@@ -35,6 +35,19 @@ Reviewer.objects.values_list('rev_name', flat=True).union(Movie.objects.values_l
 # <QuerySet [None, 'The Usual Suspects                                ', 'Amadeus                                           ', 'American Beauty                                   ', 'Alec Shaw                     ', 'Boogie Nights                                     ', 'Hannah Steele                 ', 'Neal Wruck                    ', 'The Shawshank Redemption                          ', 'Richard Adams                 ', 'Titanic                                           ', 'Krug Stillo                   ', 'Paul Monks                    ', 'Righty Sock                   ', 'Seven Samurai                                     ', 'Sasha Goldshtein              ', 'The Deer Hunter                                   ', 'Jack Malvern                  ', 'Princess Mononoke                                 ', 'Lawrence of Arabia                                ', '...(remaining elements truncated)...']>
 
 
+# 6. From the following table, write a SQL query to find all reviewers who have rated seven or more stars to their rating. Return reviewer name.
+
+Reviewer.objects.filter(rating__rev_stars__gte=7, rev_name__isnull=False).values_list('rev_name', flat=True)
+
+
+#7. From the following tables, write a SQL query to find the movies without any rating. Return movie title. 
+
+Movie.objects.exclude(mov_id__in=Rating.objects.values('mov_id')).values_list('mov_title', flat=True)
+
+
+#8. From the following table, write a SQL query to find the movies with ID 905 or 907 or 917. Return movie title.
+
+Movie.objects.filter(mov_id__in=['905', '907', '917']).values_list('mov_title', flat=True)
 
 
 
